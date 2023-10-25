d53a6819ffdbbd40d7dd81038a6c44b750604ff2
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-10-25 17:22:03 +0800

    Rubber ducking how I should layout data.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+// While not an instruction I need a method to store the value of something for an operation.
+// Something like Value(int) or Data(int) 
+enum Instruction { ADD, SUB, MULTI, DIVIDE } typedef instruction;
+
+struct VM {
+  int program_counter;
+  // I need to determine the size of the array based on the number of
+  // instructions I have. Which means this value needs to be set **AFTER** I
+  // identify how many instructions there are.
+  instruction stack[50];
+} typedef VM;
+
 int main(int argc, char **argv) {
-    printf("Hello World!");
-    return 0;
+  instruction program[] = {ADD, SUB};
+  printf("Hello World!");
+  return 0;
 }
